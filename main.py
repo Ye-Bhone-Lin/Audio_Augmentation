c51cb860dd445e320408dd9b2736e9cb6e15f0e4
Ye Bhone Lin <yebhonelin10@gmail.com>
2025-07-13 23:42:36 +0630

    completed pipeline for audio augmentation

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src.utils.pipeline import  Augmentation,HF_Augmentation
+from src.utils.spectogram_aug_pipeline import  SpectrogramAugmentation,HF_SpectrogramAugmentation
 
-pipeline = Augmentation("metadata.txt", "output_of_aug")
-pipeline.augment(1, ["noise", "pitch"])
+#pipeline = Augmentation("metadata.txt", "output_of_aug")
+#pipeline.augment(1, ["noise", "pitch"])
 
-pipeline = HF_Augmentation("/content/mig_burmese_metadata.tsv", "output_of_aug")
-pipeline.augment(1, ["noise", "pitch"])
+#pipeline = HF_Augmentation("/content/mig_burmese_metadata.tsv", "output_of_aug")
+#pipeline.augment(1, ["noise", "pitch"])
+
+#spec_pipeline = SpectrogramAugmentation("metadata.txt", "output_of_aug")
+#spec_pipeline.augment(1, ["time_mask", "freq_mask"])
+
+spec_pipeline = HF_SpectrogramAugmentation("mig_burmese_metadata.tsv", "output_of_aug")
+spec_pipeline.augment(1, ["time_mask", "freq_mask"])
